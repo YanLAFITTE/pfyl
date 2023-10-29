c7d720b017f227eb4697888b305c702755a69593
yala0 <welcome.yanlafitte@gmail.com>
2023-10-29 23:40:14 +0100

    scroll

--- app/components/RootLayout.tsx
+++ app/components/RootLayout.tsx
@@ -42,45 +42,17 @@ const RootLayoutInner: React.FC<RootLayoutInnerProps> = ({
             transition={shouldReduceMotion ? { duration: 0 } : undefined}
          
          >
-            <header>
-               <div
-                  className='absolute left-0 right-0 top-0 z-40 pt-6 lg:pt-14 '
-                  aria-hidden={expanded ? 'true' : undefined}
-                  data-inert={expanded ? '' : undefined}
-               >
-                  <Header
-                     setExpanded={setExpanded}
-                     panelId={panelId}
-                     icon={HiMenuAlt4}
-                     toggleRef={openRef}
-                     expanded={expanded}
-                     onToggle={() => {
-                        setExpanded((expanded) => !expanded);
-                        window.setTimeout(() =>
-                           closeRef.current?.focus({ preventScroll: true })
-                        );
-                     }}
-                  />
-                  <Container>
-                     <div className='h-px bg-black w-full mt-4 lg:mt-14 lg:hidden' />
-                  </Container>
-               </div>
-            </header>
-            <motion.div
-               layout
-               id={panelId}
-               style={{ height: expanded ? 'auto' : 0 }}
-               className='relative z-50 overflow-hidden  '
-               aria-hidden={expanded ? undefined : 'true'}
-               data-inert={expanded ? undefined : ''}
-            >
-               <motion.div layout>
-                  <div ref={navRef} className=' pt-6 lg:pt-14'>
+            <div className="h-full">
+               <header>
+                  <div
+                     className='absolute left-0 right-0 top-0 z-40 pt-6 lg:pt-14 '
+                     aria-hidden={expanded ? 'true' : undefined}
+                     data-inert={expanded ? '' : undefined}
+                  >
                      <Header
                         setExpanded={setExpanded}
-                        invert
                         panelId={panelId}
-                        icon={IoMdClose}
+                        icon={HiMenuAlt4}
                         toggleRef={openRef}
                         expanded={expanded}
                         onToggle={() => {
@@ -90,57 +62,87 @@ const RootLayoutInner: React.FC<RootLayoutInnerProps> = ({
                            );
                         }}
                      />
+                     <Container>
+                        <div className='h-px bg-black w-full mt-4 lg:mt-14 lg:hidden' />
+                     </Container>
                   </div>
-               </motion.div>
-               <Container>
-                  <ul className='lg:flex flex-col   pt-4 lg:pt-14'>
-                     <Link href={'/'} onClick={() => setExpanded(false)}>
-                        <li className='border-t-[1px] border-color_main py-8 group flex items-center '>
-                           <FiArrowRight className='text-3xl  text-color_main group-hover:opacity-100 opacity-0 -translate-x-10 group-hover:translate-x-0 duration-300 ease-in-out ' />
-                           <div className='text-xl font-semibold group-hover:ml-4 -ml-4  duration-300 ease-in-out uppercase  '>
-                              Home
-                           </div>
-                        </li>
-                     </Link>
-                     <Link href='/about' onClick={() => setExpanded(false)}>
-                        <li className='border-t-[1px]  border-color_main py-8 group flex items-center '>
-                           <FiArrowRight className='text-3xl text-color_main group-hover:opacity-100 opacity-0 -translate-x-10 group-hover:translate-x-0 duration-300 ease-in-out ' />
-                           <div className='text-xl font-semibold   group-hover:mx-4 -ml-4    duration-300 ease-in-out uppercase   '>
-                              About
-                           </div>
-                        </li>
-                     </Link>
-                     <Link href={'/#contact'} onClick={handleScrollToFooter}>
-                        <li className='border-t-[1px]  border-color_main py-8 group flex items-center'>
-                           <FiArrowRight className='text-3xl text-color_main  group-hover:opacity-100 opacity-0 -translate-x-10 group-hover:translate-x-0 duration-300 ease-in-out ' />
-                           <div className='text-xl font-semibold  group-hover:ml-4 -ml-4    duration-300 ease-in-out uppercase  '>
-                              Contact
-                           </div>
-                        </li>
-                     </Link>
-                  </ul>
-               </Container>
-            </motion.div>
-            <motion.div
-               layout
-               className='relative flex flex-auto overflow-hidden   bg-color_main text-color_secondary '
-            >
+               </header>
                <motion.div
                   layout
-                  className='relative isolate flex w-full flex-col '
+                  id={panelId}
+                  style={{ height: expanded ? 'auto' : 0 }}
+                  className='relative z-50 overflow-hidden  '
+                  aria-hidden={expanded ? undefined : 'true'}
+                  data-inert={expanded ? undefined : ''}
                >
-                  <main
-                     className={clsx(
-                        'w-full flex-auto sm:mb-[546px] mb-[694px] lg:mb-[618px] bg-color_main ',
-                        expanded
-                           ? ' duration-[450ms] pt-[1.5rem] '
-                           : ' duration-300 ease pt-[12.5rem] '
-                     )}
-                  >
-                     {children}
-                  </main>
+                  <motion.div layout>
+                     <div ref={navRef} className=' pt-6 lg:pt-14'>
+                        <Header
+                           setExpanded={setExpanded}
+                           invert
+                           panelId={panelId}
+                           icon={IoMdClose}
+                           toggleRef={openRef}
+                           expanded={expanded}
+                           onToggle={() => {
+                              setExpanded((expanded) => !expanded);
+                              window.setTimeout(() =>
+                                 closeRef.current?.focus({ preventScroll: true })
+                              );
+                           }}
+                        />
+                     </div>
+                  </motion.div>
+                  <Container>
+                     <ul className='lg:flex flex-col   pt-4 lg:pt-14'>
+                        <Link href={'/'} onClick={() => setExpanded(false)}>
+                           <li className='border-t-[1px] border-color_main py-8 group flex items-center '>
+                              <FiArrowRight className='text-3xl  text-color_main group-hover:opacity-100 opacity-0 -translate-x-10 group-hover:translate-x-0 duration-300 ease-in-out ' />
+                              <div className='text-xl font-semibold group-hover:ml-4 -ml-4  duration-300 ease-in-out uppercase  '>
+                                 Home
+                              </div>
+                           </li>
+                        </Link>
+                        <Link href='/about' onClick={() => setExpanded(false)}>
+                           <li className='border-t-[1px]  border-color_main py-8 group flex items-center '>
+                              <FiArrowRight className='text-3xl text-color_main group-hover:opacity-100 opacity-0 -translate-x-10 group-hover:translate-x-0 duration-300 ease-in-out ' />
+                              <div className='text-xl font-semibold   group-hover:mx-4 -ml-4    duration-300 ease-in-out uppercase   '>
+                                 About
+                              </div>
+                           </li>
+                        </Link>
+                        <Link href={'/#contact'} onClick={handleScrollToFooter}>
+                           <li className='border-t-[1px]  border-color_main py-8 group flex items-center'>
+                              <FiArrowRight className='text-3xl text-color_main  group-hover:opacity-100 opacity-0 -translate-x-10 group-hover:translate-x-0 duration-300 ease-in-out ' />
+                              <div className='text-xl font-semibold  group-hover:ml-4 -ml-4    duration-300 ease-in-out uppercase  '>
+                                 Contact
+                              </div>
+                           </li>
+                        </Link>
+                     </ul>
+                  </Container>
                </motion.div>
-            </motion.div>
+               <motion.div
+                  layout
+                  className='relative flex flex-auto overflow-hidden   bg-color_main text-color_secondary '
+               >
+                  <motion.div
+                     layout
+                     className='relative isolate flex w-full flex-col '
+                  >
+                     <main
+                        className={clsx(
+                           'w-full flex-auto sm:mb-[546px] mb-[694px] lg:mb-[618px] bg-color_main ',
+                           expanded
+                              ? ' duration-[450ms] pt-[1.5rem] '
+                              : ' duration-300 ease pt-[12.5rem] '
+                        )}
+                     >
+                        {children}
+                     </main>
+                  </motion.div>
+               </motion.div>
+            </div>
          </MotionConfig>
   
    );
